16e7d21311e8d655129704a339a67815e611e488
wk1219 <sjms1215@naver.com>
2020-04-06 23:15:44 +0900

    update db_parsing (db_row)

--- db_parsing.py
+++ db_parsing.py
@@ -1,11 +1,19 @@
-import re
-
 f = open('co_block2.txt','rb')
 lines = f.readlines()
+db_row = []
 for line in lines:
     item = line.split()
     rowid = item[0]
-    print(rowid)
+    time = item[1]
+    user = item[2]
+    rollback = str(item[3])
+    wid = item[4]
+    x = item[5]
+    y = item[6]
+    z = item[7]
+    db_row = [rowid, time, user, rollback, wid, x, y, z]
+    if rollback == "b'1,'":
+        print(db_row)
 
 
 
